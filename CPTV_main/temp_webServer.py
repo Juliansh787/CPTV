6196ce11aebbe2e1f48786ce8a6d8fecfbdf8678
Julian_SehoonKim787 <59464137+Juliansh787@users.noreply.github.com>
2021-07-13 20:48:20 +0900

    Update temp_webServer.py

--- CPTV_main/temp_webServer.py
+++ CPTV_main/temp_webServer.py
@@ -1,9 +1,8 @@
 import socket                   # Import socket module
-import os
 
 s = socket.socket()             # Create a socket object
-host = "192.168.0.6"   # Get local machine name
-port = 22042                    # Reserve a port for your service every new transfer wants a new port or you must wait.
+host = '192.168.0.6'   # Get local machine name
+port = 22041                    # Reserve a port for your service every new transfer wants a new port or you must wait.
 s.bind((host, port))            # Bind to the port
 s.listen(5)  # Now wait for client connection.
 print('Server listening....')
